dfd92b100aa505b9bd57508d4fd02d8c11d14e7d
Mohamed Elidrissi <67818913+elidrissidev@users.noreply.github.com>
2024-06-06 11:28:32 +0100

    Fix PHPUnit testsuite

--- test/TransactionApiTest.php
+++ test/TransactionApiTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace  ShipStream\SpsCommerce\Tests;
 
+use Exception;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
@@ -12,8 +13,6 @@ class TransactionApiTest extends TestCase
 {
 
     /**
-     * @test
-     *
      * Get JSON response data of API `https://api.spscommerce.com/transactions/v5/data/out/?limit=200&cursor=1iV7qUkBbZVTXiHkle&entryNamePrefix=CA`
      * @throws Exception
      * @throws GuzzleException
@@ -30,13 +29,13 @@ class TransactionApiTest extends TestCase
         $client = new Client(['handler' => $handlerStack]);
 
         $requestResponse = $client->request("GET", '/');
+        $this->assertIsObject($requestResponse);
         $data = json_decode($requestResponse->getBody()->getContents());
-        $this->assertEquals(true, is_object($requestResponse));
         $this->assertEquals(1000, $data->paging->limit);
         $this->assertEquals("1iV7qUkBbZVTXiHkle", $data->paging->next->cursor);
         $this->assertEquals("/out/CA584615-1-v7.7-BulkImport.xml", $data->results[0]->path);
-        $this->assertEquals("/file", $data->results[0]->type);
-        $this->assertEquals("application/json", $requestResponse->getHeader('Content-Type'));
+        $this->assertEquals("file", $data->results[0]->type);
+        $this->assertEquals(["application/json"], $requestResponse->getHeader('Content-Type'));
         $this->assertInstanceOf('GuzzleHttp\\Psr7\\Response', $requestResponse);
         $this->assertEquals(200, $requestResponse->getStatusCode());
 
